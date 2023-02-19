3f4ed7040bb24f6f931c8f0018e29b3bde6f1743
Jing <1210353674@qq.com>
2023-02-19 18:28:59 +0800

    Redis实现生成全局唯一Id

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,45 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    private static final long BEGIN_TIMESTAMP = 1672531200L;
+
+    //序列号的位数
+    private static final int COUNT_BITS = 32;
+
+    public long nextId(String keyPrefix) {
+        //生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        //获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+
+        //生成序列号
+        Long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        return timestamp << COUNT_BITS | count;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.of(2023, 1, 1, 0, 0, 0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(second);
+    }
+
+}
